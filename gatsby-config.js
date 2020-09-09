a4d4b775d5311d14e42de22222d3f1ce817821f4
Gabriel Hercules <gabrielsyze@gmail.com>
2020-09-08 21:03:02 -0300

    Cleanup Starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gabriel Hercules | Brand Experience Designer`,
+    description: `Hey! Eu sou o Gabriel. E desenvolvo experiências motivadoras para marcas, produtos e pessoas.`,
+    author: `@Me`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
