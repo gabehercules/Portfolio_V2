5b547b201c72c2267456168c20bea10b4a24a896
Gabriel Hercules <gabrielsyze@gmail.com>
2021-02-01 19:12:40 -0300

    removed faq section

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -5,7 +5,6 @@ import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 import ContactHeading from "../components/ContactHeading"
-import ContactInfo from "../components/ContactInfo"
 import ContactForm from "../components/ContactForm"
 
 const contactPage = () => (
@@ -13,7 +12,6 @@ const contactPage = () => (
     <SEO title="Contato" description="Entre em contato preenchendo o formulário ou pelo whatsapp (11)94480-1999"/>
     <ContactHeading />
     <ContactForm />
-    <ContactInfo />
   </Layout>
 )
 
